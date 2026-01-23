bad56780a6f6a05425e968725262226c27d38098
James Code <jamesdxb@meta.com>
2026-01-22 16:00:16 -0800

    Update request_processing.rs
    Signed-off-by: James Code <jamesdxb@meta.com>

--- benches/request_processing.rs
+++ benches/request_processing.rs
@@ -96,6 +96,7 @@ fn default_chat_completion_request() -> ChatCompletionRequest {
         echo: None,
         reasoning_effort: None,
         include_reasoning: true,
+        structured_output: None,
     }
 }
 
